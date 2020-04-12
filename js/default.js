b6733dfe675f60075a5fe5ff3eb314de0f203e49
Aleksander <adishnica31@gmail.com>
2020-04-13 01:04:58 +0200

    Double diagonal numbers in JS array

--- js/default.js
+++ js/default.js
@@ -1,3 +1,5 @@
+"use strict";
+
 let a = document.getElementById('root');
 
 let array = [
@@ -5,5 +7,12 @@ let array = [
 	[ 4, 5, 6 ],
 	[ 7, 8, 9 ],
 ];
+
+for(let i=0; i < array.length; i++){
+	array[i][i] *= 2;
+	array[i][array.length-1-i] *= 2;
+}
+
+console.log(array);
 
 a.innerHTML = array;
